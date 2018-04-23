1df3ef193c79558e5463d5e7ea12f4b4902f42ca
yangjifei <1021967981@qq.com>
2018-04-23 14:21:28 +0800

    Create GetOneRandomItemByWeight.cs

---
+++ GetOneRandomItemByWeight.cs
@@ -0,0 +1,20 @@
+    public static int GetOneRandomItemByWeight(int[] items)
+    {
+        float maxPower = 0;
+        foreach (var item in items)
+        {
+            maxPower += item;
+        }
+        float randomResult = UnityEngine.Random.Range(0, maxPower);
+        float tempValue = 0;
+        for (int i = 0; i < items.Length; i++)
+        {
+            var item = items[i];
+            tempValue += item;
+            if (randomResult <= tempValue)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
